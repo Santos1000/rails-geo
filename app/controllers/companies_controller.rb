599a7113c2159749a3755dbb8aa3dbbb9cef7a9f
barbara santos <barbaransantos1@gmail.com>
2022-04-14 21:07:53 -0300

    iteration data_api index

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -21,7 +21,6 @@ class CompaniesController < ApplicationController
   private
 
   def request_api
-
     url = "https://yh-finance.p.rapidapi.com/auto-complete?q=tesla&region=US"
     data = HTTParty.get(
       url,
@@ -29,12 +28,8 @@ class CompaniesController < ApplicationController
         'X-RapidAPI-Key' => ENV.fetch('RAPIDAPI_API_KEY')
       }
     )
-    binding.pry
-
-    data = JSON.parse(response.body)
+    #data = JSON.parse(response.body)
     @quotes = data['quotes']
-
-
   end
 
 
@@ -45,50 +40,3 @@ class CompaniesController < ApplicationController
   # end
 end
 
-  #   data_exchDisp   = []
-  #   data_longname  = []
-  #   data_symbol  = []
-  #   data_region = []
-# ------------------------
-  # def search_companies(symbol)
-  #   request_api(
-  #     "https://restcountries-v1.p.rapidapi.com/name/#{URI.encode(symbol)}"
-  #   )
-
-  #     # puts "#{user["name"]} - #{user["bio"]}"
-
-  # end
-
-# ------------------------
-  # if data_json.select{ |key| key["symbol"] == params[:query] }.empty?
-  # flash[:alert] = "This coin doesn't exist, type again."
-  # else
-  # response = data_json.select{ |key| key["symbol"] == params[:query] }.first
-  # @coin = response.transform_keys(&:to_sym)
-  # @btc = get_btc
-  # end
-# ------------------------
-
-# require "json"
-# require "open-uri"
-
-# url = "https://yh-finance.p.rapidapi.com/auto-complete?q=tesla&region=US"
-# user_serialized = URI.open(url).read
-# user = JSON.parse(user_serialized)
-
-# puts "#{user["name"]} - #{user["bio"]}"
-
-# -------------------------
-
-# url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd'
-  # if params[:query].present?
-  # data = HTTParty.get(url).body
-  # data_json = JSON.parse(data)
-    # if data_json.select{ |key| key["symbol"] == params[:query] }.empty?
-    # flash[:alert] = "This coin doesn't exist, type again."
-    # else
-    # response = data_json.select{ |key| key["symbol"] == params[:query] }.first
-  # @coin = response.transform_keys(&:to_sym)
-
-  # end
-# end
