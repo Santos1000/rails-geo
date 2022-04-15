426c29c5321adb35810b80714fb312146a0cc67b
barbara santos <barbaransantos1@gmail.com>
2022-04-14 23:02:18 -0300

    adjust view and start search

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,21 +2,40 @@ class CompaniesController < ApplicationController
 
   def index
     request_api
-    # companies = find_company(params[:company])
-    # unless companies
-    #   flash[:alert] = 'company not found'
-    #   return render action: :index
-    # end
-    # @company = companies.first
-  end
-
-  def show
-
   end
 
   def about
-
+    search_company
   end
+
+  def search_company
+    # url = "https://yh-finance.p.rapidapi.com/auto-complete?q=#{:query_name}&region=#{:query_region}"
+
+
+
+
+
+    
+    # data = HTTParty.get(
+    #   url,
+    #   headers: {
+    #     'X-RapidAPI-Key' => ENV.fetch('RAPIDAPI_API_KEY')
+    #   }
+    # )
+    # #data = JSON.parse(response.body)
+    # @search_quotes = data['quotes']
+
+
+
+
+    # companies = search_company(params[:company])
+      # unless companies
+      #   flash[:alert] = 'company not found'
+      #   return render action: :index
+      # end
+    # @company = companies.first
+  end
+
 
   private
 
@@ -32,11 +51,7 @@ class CompaniesController < ApplicationController
     @quotes = data['quotes']
   end
 
-
-  # def find_company(symbol)
-  #   request_api(
-  #     "https://yh-finance.p.rapidapi.com/auto-complete?q=#{URI.encode_www_form(symbol)}"
-  #   )
-  # end
+  def company_params
+    params.require(:company_share).permit(:query_name, :query_region)
+  end
 end
-
