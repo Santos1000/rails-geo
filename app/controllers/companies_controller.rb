e6d702843e4ef8fd8dd847f6254b5862f9fa839d
barbara santos <barbaransantos1@gmail.com>
2022-04-15 15:12:56 -0300

    add a search function

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,46 +1,19 @@
 class CompaniesController < ApplicationController
 
   def index
-    request_api
+    if params[:query].present?
+      @quotes_search = request_api(params[:query])
+    end
   end
 
-  def about
-    search_company
+  def show
+    # search_company
   end
-
-  def search_company
-    # url = "https://yh-finance.p.rapidapi.com/auto-complete?q=#{:query_name}&region=#{:query_region}"
-
-
-
-
-
-    
-    # data = HTTParty.get(
-    #   url,
-    #   headers: {
-    #     'X-RapidAPI-Key' => ENV.fetch('RAPIDAPI_API_KEY')
-    #   }
-    # )
-    # #data = JSON.parse(response.body)
-    # @search_quotes = data['quotes']
-
-
-
-
-    # companies = search_company(params[:company])
-      # unless companies
-      #   flash[:alert] = 'company not found'
-      #   return render action: :index
-      # end
-    # @company = companies.first
-  end
-
 
   private
 
-  def request_api
-    url = "https://yh-finance.p.rapidapi.com/auto-complete?q=tesla&region=US"
+  def request_api(search)
+    url = "https://yh-finance.p.rapidapi.com/auto-complete?q=#{search}"
     data = HTTParty.get(
       url,
       headers: {
@@ -48,10 +21,9 @@ class CompaniesController < ApplicationController
       }
     )
     #data = JSON.parse(response.body)
-    @quotes = data['quotes']
-  end
 
-  def company_params
-    params.require(:company_share).permit(:query_name, :query_region)
+    # binding.pry
+    @quotes = data['quotes']
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'companies#about'
+  root to: 'companies#index'
   # get 'index', to: 'companies#index'
-  get 'about', to: 'companies#about'
   get '/index' => 'companies#index'
+  get 'show', to: 'companies#show'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
