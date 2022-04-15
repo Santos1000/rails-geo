d727d06b1a073923c8d89b46b970d1d1c7c8ea31
barbara santos <barbaransantos1@gmail.com>
2022-04-15 20:34:00 -0300

    saving route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'companies#index'
   # get 'index', to: 'companies#index'
-  get '/index' => 'companies#index'
-  get 'show', to: 'companies#show'
+  get '/index', to: 'companies#index'
+  get '/show', to: 'companies#show'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
